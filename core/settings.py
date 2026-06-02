cfbb39b700acd5ce4d1622ae54df3646074f10fb
Jenis-das <jenisdas254@gmail.com>
2026-06-02 14:29:35 +0530

    Update static files backend configuration for Django 5.2

--- core/settings.py
+++ core/settings.py
@@ -117,8 +117,20 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
+# Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Django 5.2 updated storage settings! Use STORAGES instead of the old STATICFILES_STORAGE line:
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
